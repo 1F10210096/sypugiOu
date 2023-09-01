75e664f3288fdf58440a846d0604f2c160eda4cd
1F10210096 <s1F102100962@iniad.org>
2023-09-01 20:13:36 +0900

    クリックできる

--- client/src/components/ShogiBoard.tsx
+++ client/src/components/ShogiBoard.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import styles from './index.module.css';
 
 const boardData = [
@@ -13,17 +14,29 @@ const boardData = [
 ];
 
 const ShogiBoard = () => {
+  const [selectedPiece, setSelectedPiece] = useState<string | null>(null);
+
+  const handleSquareClick = (piece: string | null) => {
+    setSelectedPiece(piece);
+    console.log(piece);
+  };
   return (
     <div className={styles.board}>
       {boardData.map((row, rowIndex) => (
         <div key={rowIndex} className={styles.row}>
           {row.map((piece, colIndex) => (
-            <div key={colIndex} className={styles.square}>
+            <div
+              key={colIndex}
+              className={styles.square}
+              onClick={() => handleSquareClick(piece)} // クリック時のハンドラを追加
+            >
               {piece !== null ? <div className={styles.piece}>{piece}</div> : null}
             </div>
           ))}
         </div>
       ))}
+      {/* 選択された駒の表示 */}
+      {selectedPiece !== null && <div>選択された駒: {selectedPiece}</div>}
     </div>
   );
 };
