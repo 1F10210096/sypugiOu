e1bc7c09d0d70d62526bf4bddd8abc4d3fd6cc30
1F10210096 <s1F102100962@iniad.org>
2023-09-01 22:33:06 +0900

    飛車の候補地2

--- client/src/components/ShogiBoard.tsx
+++ client/src/components/ShogiBoard.tsx
@@ -18,54 +18,88 @@ const clickBoardData = Array(9).fill(Array(9).fill(0));
 const ShogiBoard = () => {
   const [boardData, setBoardData] = useState(initialBoardData);
   const [selectedPiece, setSelectedPiece] = useState<string | null>(null);
-  const [candidateSquare, setCandidateSquare] = useState<{ row: number; col: number } | null>(null);
+  const [candidateSquare, setCandidateSquare] = useState<{ row: number; col: number }[]>([]);
 
+  // eslint-disable-next-line complexity
   const handleSquareClick = (piece: string | null, rowIndex: number, colIndex: number) => {
     // 歩の駒をクリックした場合、候補地を計算して移動
     console.log(piece);
     console.log(candidateSquare);
 
-    console.log(candidateSquare?.col);
-    console.log(candidateSquare?.row);
-    if (
-      piece === '歩' &&
-      candidateSquare !== null && // candidateSquareがnullでないことを確認
-      candidateSquare.row === rowIndex && // 候補地の行とクリックした場所の行が一致
-      candidateSquare.col === colIndex // 候補地の列とクリックした場所の列が一致
-    ) {
-      // 歩を移動する
-      console.log('a');
-      const updatedBoardData = [...boardData];
-      updatedBoardData[candidateSquare.row][candidateSquare.col] = null; // 候補地をクリア
-      updatedBoardData[rowIndex][colIndex] = '歩'; // クリックした場所に歩を移動
-      setBoardData(updatedBoardData);
-      setSelectedPiece(null); // selectedPieceをクリア
-      setCandidateSquare(null); // candidateSquareをクリア
+    console.log(rowIndex);
+    console.log(colIndex);
+
+    console.log('b');
+    setSelectedPiece(piece);
+    // 歩の場合、候補地を計算して表示
+    if (piece === '歩' && rowIndex > 0) {
+      setCandidateSquare([{ row: rowIndex - 1, col: colIndex }]); // 配列として候補地を設定
     } else {
-      console.log('b');
-      setSelectedPiece(piece);
-      // 歩の場合、候補地を計算して表示
-      if (piece === '歩' && rowIndex > 0) {
-        setCandidateSquare({ row: rowIndex - 1, col: colIndex });
+      setCandidateSquare([]); // それ以外の場合は空の配列を設定
+    }
+    
+     // 飛車の場合、候補地を計算して表示
+  if (piece === '飛') {
+    const candidateSquares: { row: number; col: number }[] = [];
+    
+    // 縦方向の候補地を計算
+    for (let i = rowIndex - 1; i >= 0; i--) {
+      // eslint-disable-next-line max-depth
+      if (boardData[i][colIndex] === null) {
+        candidateSquares.push({ row: i, col: colIndex });
       } else {
-        setCandidateSquare(null); // それ以外の場合は候補地をクリア
+        break;
       }
     }
-  };
-
-  const handleCandidateClick = (rowIndex: number, colIndex: number) => {
-    console.log('a');
-
-    if (selectedPiece === '歩' && candidateSquare !== null) {
-      // 歩を移動する
-      const updatedBoardData = [...boardData];
-      updatedBoardData[candidateSquare.row][candidateSquare.col] = '歩';
-      updatedBoardData[candidateSquare.row + 1][candidateSquare.col] = null;
-      setBoardData(updatedBoardData);
-      setSelectedPiece(null);
-      setCandidateSquare(null);
+    for (let i = rowIndex + 1; i < 9; i++) {
+      // eslint-disable-next-line max-depth
+      if (boardData[i][colIndex] === null) {
+        candidateSquares.push({ row: i, col: colIndex });
+      } else {
+        break;
+      }
     }
+    
+    // 横方向の候補地を計算
+    for (let i = colIndex - 1; i >= 0; i--) {
+      // eslint-disable-next-line max-depth
+      if (boardData[rowIndex][i] === null) {
+        candidateSquares.push({ row: rowIndex, col: i });
+      } else {
+        break;
+      }
+    }
+    for (let i = colIndex + 1; i < 9; i++) {
+      // eslint-disable-next-line max-depth
+      if (boardData[rowIndex][i] === null) {
+        candidateSquares.push({ row: rowIndex, col: i });
+      } else {
+        break;
+      }
+    }
+    
+    // 計算された候補地を設定
+    setCandidateSquare(candidateSquares);
+  } else {
+    // それ以外の場合は候補地をクリア
+    setCandidateSquare([]);
+  }
   };
+
+  // const handleCandidateClick = (rowIndex: number, colIndex: number) => {
+  //   console.log('a');
+
+  //   if (selectedPiece === '歩' && candidateSquare !== null) {
+  //     // 歩を移動する
+  //     const updatedBoardData = [...boardData];
+  //     updatedBoardData[candidateSquare.row][candidateSquare.col] = '歩';
+  //     updatedBoardData[candidateSquare.row + 1][candidateSquare.col] = null;
+  //     setBoardData(updatedBoardData);
+  //     setSelectedPiece(null);
+  //     setCandidateSquare(null);
+  //   }
+  // };
+
 
   return (
     <div className={styles.board}>
@@ -75,19 +109,12 @@ const ShogiBoard = () => {
             <div key={colIndex} className={styles.square}>
               {piece !== null ? (
                 <div
-                  className={styles.piece}
+                  className={styles.nk_koma}
                   onClick={() => handleSquareClick(piece, rowIndex, colIndex)}
                 >
                   {piece}
                 </div>
-              ) : (
-                candidateSquare !== null ? (
-                  <div
-                    className={styles.candidate}
-                    onClick={() => handleCandidateClick(rowIndex, colIndex)}
-                  />
-                ) : null
-              )}
+              ) : null}
             </div>
           ))}
         </div>
