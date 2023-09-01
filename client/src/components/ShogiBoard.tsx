fb5139f983d7e062fc1a8c2d16b7e44e15b83c3d
1F10210096 <s1F102100962@iniad.org>
2023-09-01 20:33:58 +0900

    歩の候補地が出せるように

--- client/src/components/ShogiBoard.tsx
+++ client/src/components/ShogiBoard.tsx
@@ -15,22 +15,36 @@ const boardData = [
 
 const ShogiBoard = () => {
   const [selectedPiece, setSelectedPiece] = useState<string | null>(null);
+  const [candidateSquare, setCandidateSquare] = useState<{ row: number; col: number } | null>(null);
 
-  const handleSquareClick = (piece: string | null) => {
+  const handleSquareClick = (piece: string | null, rowIndex: number, colIndex: number) => {
     setSelectedPiece(piece);
-    console.log(piece);
+
+    // 歩の駒をクリックした場合、候補地を計算して表示
+    if (piece === '歩' && rowIndex > 0) {
+      setCandidateSquare({ row: rowIndex - 1, col: colIndex });
+    } else {
+      setCandidateSquare(null); // それ以外の場合は候補地をクリア
+    }
   };
+
   return (
     <div className={styles.board}>
       {boardData.map((row, rowIndex) => (
         <div key={rowIndex} className={styles.row}>
           {row.map((piece, colIndex) => (
-            <div
-              key={colIndex}
-              className={styles.square}
-              onClick={() => handleSquareClick(piece)} // クリック時のハンドラを追加
-            >
-              {piece !== null ? <div className={styles.piece}>{piece}</div> : null}
+            <div key={colIndex} className={styles.square}>
+              {piece !== null ? (
+                <div
+                  className={styles.piece}
+                  onClick={() => handleSquareClick(piece, rowIndex, colIndex)}
+                >
+                  {piece}
+                </div>
+              ) : null}
+              {candidateSquare !== null &&
+                candidateSquare.row === rowIndex &&
+                candidateSquare.col === colIndex && <div className={styles.candidate} />}
             </div>
           ))}
         </div>
